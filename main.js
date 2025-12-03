00cf93d95f84a364d838839b164db6f6b5f04b07
Mcaldas11 <miguelmeniciocaldas@gmail.com>
2025-12-03 21:16:55 +0000

    base configuration for next projecct

---
+++ main.js
@@ -0,0 +1,7 @@
+import * as THREE from "three";
+
+/*********************
+ * SCENE
+ * *******************/
+// create an empty scene, that will hold all our elements such as objects, cameras and lights
+const scene = new THREE.Scene();
